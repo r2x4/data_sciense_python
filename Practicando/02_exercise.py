fbb55c3bd943a4b32eafead193a5d2368c1f4284
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-05-02 16:09:25 -0500

    Commit1

--- Practicando/02_exercise.py
+++ Practicando/02_exercise.py
@@ -7,3 +7,5 @@ minutos = (segundos % 3600) // 60
 segundos_restantes = segundos % 60
 
 print(f"{segundos} segundos son {horas} horas, {minutos} minutos y {segundos_restantes} segundos.")
+
+### Ejercicio
