4efa571e946fb96aafbfa13cfde88cc8f45d81f5
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-05-02 11:33:02 -0500

    camino

---
+++ Practicando/01_exercise.py
@@ -0,0 +1,10 @@
+### Ejercicios Python
+
+### Ejercicio 1
+
+num1 = int(input("Ingresa primer Numero:"))
+num2 = int(input("Ingresa segundo numero: "))
+
+def suma(a, b):
+    return a + b
+print("El valor de la suma es:", suma(num1, num2))
