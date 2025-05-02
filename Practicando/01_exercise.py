1f83344a050047cd9bd3cb8afb2b30ca25e1a0bb
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-05-02 11:52:55 -0500

    nuevo1

--- Practicando/01_exercise.py
+++ Practicando/01_exercise.py
@@ -1,5 +1,7 @@
 ### Ejercicios Python
 
+import math
+
 ### Ejercicio 1
 
 num1 = int(input("Ingresa primer Numero:"))
@@ -8,3 +10,24 @@ num2 = int(input("Ingresa segundo numero: "))
 def suma(a, b):
     return a + b
 print("El valor de la suma es:", suma(num1, num2))
+
+### Ejercicio 2
+
+def km_a_millas(km):
+    return km * 0.621371
+
+kilometros = float(input("Ingresa distancia en Kilometros: "))
+millas = km_a_millas(kilometros)
+
+print(f'{kilometros} kilometros son {millas} millas.')
+
+### Ejercicio 3
+
+def area_circulo(radio):
+    return  math.pi * radio ** 2
+
+radio = float(input("Ingresa el radio del circulo: "))
+
+area = area_circulo(radio)
+
+print(f'El area del circulo con radio {radio} es: {area:.2f}')
