482cdecafd8dad032ebb8ca84c21daa4bac7f3b2
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-05-02 16:59:05 -0500

    fin

--- Practicando/02_exercise.py
+++ Practicando/02_exercise.py
@@ -19,3 +19,15 @@ elif numero< 0:
 else:
     print('El numero es cero')
     
+### Ejercicio 7
+
+letra = input('Ingresa una letra: ').lower()
+
+if len(letra) == 1 and letra.isalpha():
+    if letra in 'aeiou':
+        print('Es una Vocal')
+    else:
+        print("Es una consonante")
+else:
+    print("Entrada no valida. Debes  ingresar una  solo una letra")
+
