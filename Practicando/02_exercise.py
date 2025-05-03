8c8c601d3074f1262ae4e1833ad21bc562b69cd4
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-05-03 13:38:03 -0500

    colombia

--- Practicando/02_exercise.py
+++ Practicando/02_exercise.py
@@ -7,3 +7,26 @@ minutos = (segundos % 3600) // 60
 segundos_restantes = segundos % 60
 
 print(f"{segundos} segundos son {horas} horas, {minutos} minutos y {segundos_restantes} segundos.")
+
+### Ejercicio 6
+## calculadora basica
+
+num1 = float(input('Ingresa primer numero: '))
+num2 = float(input('Ingresa Segundo numero: '))
+operacion = input('Ingrese operacion en texto en minus.: ')
+resultado = 0
+if operacion == 'suma':
+    resultado = num1 + num2
+elif operacion == 'resta':
+    resultado = num1 - num2
+elif operacion == 'multiplicacion':
+    resultado = num1 * num2
+elif operacion == 'division':
+    resultado = num1/num2
+elif operacion == 'potencia':
+    resultado = num1 ** num2
+else:
+    print('Ingrese una operacion valida')
+
+print(f'el resultado de la {operacion} es: {resultado}')
+
