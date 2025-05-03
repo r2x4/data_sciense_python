215bfaf55cd7449294b433890042f315c52592f6
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-05-03 13:49:50 -0500

    padse

--- Practicando/02_exercise.py
+++ Practicando/02_exercise.py
@@ -8,6 +8,29 @@ segundos_restantes = segundos % 60
 
 print(f"{segundos} segundos son {horas} horas, {minutos} minutos y {segundos_restantes} segundos.")
 
+### Ejercicio
+
+numero = float(input('Ingresa un numero: '))
+
+if numero > 0:
+    print('El numero es positivo')
+elif numero < 0:
+    print('El numero es negativo')
+else:
+    print('El numero es cero')
+
+### Ejercicio 7
+
+letra = input('Ingresa una letra: ').lower()
+
+if len(letra) == 1 and letra.isalpha():
+    if letra in 'aeiou':
+        print('Es una Vocal')
+    else:
+        print("Es una consonante")
+else:
+    print("Entrada no valida. Debes  ingresar una  solo una letra")
+
 ### Ejercicio 6
 ## calculadora basica
 
